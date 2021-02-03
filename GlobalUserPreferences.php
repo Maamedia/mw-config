5201a497f3c18aa650e10b1a35143cbc311792dc
maantietaja <76481138+maantietaja@users.noreply.github.com>
2021-02-03 09:06:11 +0200

    Create GlobalUserPreferences.php

---
+++ GlobalUserPreferences.php
@@ -0,0 +1,21 @@
+<?php
+
+// Global User Preferences //
+
+// Echo 
+
+$wgDefaultUserOptions['echo-show-alert'] = true;
+$wgDefaultUserOptions['echo-email-frequency'] = '0';
+
+// VisualEditor
+$wgDefaultUserOptions['visualeditor-enable'] = 1;
+$wgDefaultUserOptions['visualeditor-editor'] = 'visualeditor';
+$wgDefaultUserOptions['visualeditor-newwikitext'] = 1;
+
+// CodeMirror
+$wgDefaultUserOptions['usecodemirror'] = 1;
+
+
+
+// Others
+$wgUseGlobalSiteCssJs = true;
