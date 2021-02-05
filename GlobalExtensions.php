165010bcc60143a11fbd0a2b5379f3f8f52c4d5c
maantietaja <76481138+maantietaja@users.noreply.github.com>
2021-02-05 17:50:38 +0200

    update extensions

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -1,18 +1,19 @@
 <?php
 
 // Global Extensions //
-
 wfLoadExtensions( [
 	'AbuseFilter',
 	'AdvancedMeta',
 	'AdvancedSearch',
 	'BetaFeatures',
 	'CategoryTree',
+	'Babel',
 	'CentralAuth',
 	'CentralNotice', 
 	'CheckUser',
 	'CodeEditor',
 	'CodeMirror',
+	'CodeReview',
 	'InputBox', 
 	'ImageMap', 
 	'Interwiki', 
@@ -33,7 +34,12 @@ wfLoadExtensions( [
 	'MassMessage', 
 	'MultimediaViewer',
 	'Nuke',
+	'NewsTicker',
+	'TemplateData',
+	'TemplateStyles',
 	'Poem',
+	'OATHAuth',
+	'OAuth',
 	'ParserFunctions', 
 	'ProtectSite',
 	'ReplaceText',
@@ -43,6 +49,21 @@ wfLoadExtensions( [
 	'WikiEditor',
 	'VisualEditor',
 	'MobileFrontend',
-        'JsonConfig',
-        'mediawiki-extensions-Scribunto-master',
+    'JsonConfig',
+    'mediawiki-extensions-Scribunto-master',
+    'mediawiki-extensions-MassEditRegex-master',
 ] );
+
+// Wikibase //
+$wgEnableWikibaseRepo = false;
+$wgEnableWikibaseClient = true;
+require_once "$IP/extensions/Wikibase/client/WikibaseClient.php";
+require_once "$IP/extensions/Wikibase/client/ExampleSettings.php";
+
+$wgWBClientSettings['repoUrl'] = 'https://data.maantietaja.org';
+$wgWBClientSettings['repoScriptPath'] = '';
+$wgWBClientSettings['repoArticlePath'] = '/wiki/$1';
+$wgWBClientSettings['repositories']['']['repoDatabase'] = 'maantiet_mw19416';
+$wgWBClientSettings['repositories']['']['changesDatabase'] = 'maantiet_mw19416';
+$wgWBClientSettings['siteLinkGroups'] = [ 'maantiet_test', 'maantiet_community', 'maantiet_beta' ];
+$wgWBClientSettings['siteGlobalID'] = 'en';
