368fd37cfe26e4ae1638ea4a5d91e4d25b17d236
syma51 <78367257+syma51@users.noreply.github.com>
2021-03-25 14:52:57 +0700

    Update GlobalConfig.php

--- GlobalConfig.php
+++ GlobalConfig.php
@@ -1,13 +1,8 @@
 <?php
-include('/var/www/privateinfo.php');
-    $wgEnableWikibaseRepo = false;
-    $wgEnableWikibaseClient = true;
-    require_once "$IP/extensions/Wikibase/client/WikibaseClient.php";
-    require_once "$IP/extensions/Wikibase/client/ExampleSettings.php";
-    $wgWBClientSettings['repoUrl'] = 'https://data.maamedia.org';
-    $wgWBClientSettings['repoScriptPath'] = '';
-    $wgWBClientSettings['repoArticlePath'] = '/wiki/$1';
-    $wgWBClientSettings['repositories']['']['repoDatabase'] = 'datawiki';
-    $wgWBClientSettings['repositories']['']['changesDatabase'] = 'datawiki';
-    $wgWBClientSettings['siteLinkGroups'] = [ 'datawiki' ];
-    $wgWBClientSettings['TOOLBAR'] = [ 'Wiki' ];
+$wgWBClientSettings['repoUrl'] = 'https://data.maamedia.org';
+$wgWBClientSettings['repoScriptPath'] = '';
+$wgWBClientSettings['repoArticlePath'] = '/wiki/$1';
+$wgWBClientSettings['repositories']['']['repoDatabase'] = 'datawiki';
+$wgWBClientSettings['repositories']['']['changesDatabase'] = 'datawiki';
+$wgWBClientSettings['siteLinkGroups'] = [ 'datawiki' ];
+$wgWBClientSettings['TOOLBAR'] = [ 'Wiki' ];
