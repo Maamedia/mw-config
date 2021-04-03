e069f22afe08ddf45dd245ac62141a45791bebba
syma51 <78367257+syma51@users.noreply.github.com>
2021-04-04 00:37:45 +0700

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -11,36 +11,18 @@ $wgShowDBErrorBacktrace = true;
 # Choose the right wiki to show
 if ( defined( 'MW_DB' ) ) {
     $wikiId = MW_DB;
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'meta.maamedia.org') {
-    $wikiId = 'metawiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'steward.maamedia.org') {
-    $wikiId = 'stewardwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'fi.masymanwiki.org') {
-    $wikiId = 'fiwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'data.maamedia.org') {
-    $wikiId = 'datawiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'login.maamedia.org') {
-    $wikiId = 'loginwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'test.maamedia.org') {
-    $wikiId = 'testwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'code.maamedia.org') {
-    $wikiId = 'codewiki';
 } elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'commons.maamedia.org') {
     $wikiId = 'commonswiki';
+} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'data.maamedia.org') {
+    $wikiId = 'datawiki';
+} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'meta.maamedia.org') {
+    $wikiId = 'metawiki';
 } elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'en.masymanwiki.org') {
     $wikiId = 'enwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'es.masymanwiki.org') {
-    $wikiId = 'eswiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'de.masymanwiki.org') {
-    $wikiId = 'dewiki';
 } elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'fi.masymanwiki.org') {
     $wikiId = 'fiwiki';
 } elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'id.masymanwiki.org') {
     $wikiId = 'idwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'sv.masymanwiki.org') {
-    $wikiId = 'svwiki';
-} elseif (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'no.masymanwiki.org') {
-    $wikiId = 'nowiki';
 } else {
     echo 'Unable to determine $wikiId'."\n";
     exit(1);
@@ -58,20 +40,12 @@ $wgServer = [
     // if you want to allow also usage of https, just use '//localhost'
     // and set 'http://localhost' at 'wgCanonicalServer'
     'default' => 'https://localhost',
-    'metawiki' => 'https://meta.maamedia.org',
-    'loginwiki' => 'https://login.maamedia.org',
-    'stewardwiki' => 'https://steward.maamedia.org',
-    'testwiki' => 'https://test.maamedia.org',
-    'codewiki' => 'https://code.maamedia.org',
     'commonswiki' => 'https://commons.maamedia.org',
     'datawiki' => 'https://data.maamedia.org',
+    'metawiki' => 'https://meta.maamedia.org',
     'enwiki' => 'https://en.masymanwiki.org',
-    'eswiki' => 'https://es.masymanwiki.org',
-    'dewiki' => 'https://de.masymanwiki.org',
-    'idwiki' => 'https://id.masymanwiki.org',
     'fiwiki' => 'https://fi.masymanwiki.org',
-    'svwiki' => 'https://sv.masymanwiki.org',
-    'nowiki' => 'https://no.masymanwiki.org',
+    'idwiki' => 'https://id.masymanwiki.org',
 ];
 
 ## The URL path to static resources (images, scripts, etc.)
@@ -107,20 +81,12 @@ $wgDBmysql5 = false;
 
 # Read wiki lists
 $wgLocalDatabases = array(
-    'metawiki',
-    'loginwiki',
-    'stewardwiki',
-    'testwiki',
-    'codewiki',
     'commonswiki',
     'datawiki',
+    'metawiki',
     'enwiki',
-    'eswiki',
-    'dewiki',
-    'idwiki',
     'fiwiki',
-    'svwiki',
-    'nowiki',
+    'idwiki',
 );
 
 include('LocalConfig.php');
