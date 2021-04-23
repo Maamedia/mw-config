f908e4594ccd9dd99204f9d46b79027dbd519a79
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-04-23 16:49:01 +0300

    Update InitialiseSettings.php

--- InitialiseSettings.php
+++ InitialiseSettings.php
@@ -1138,7 +1138,7 @@ $wgConf->settings = [
 	// changes to the default across all overrides --Andrew 2009-04-28
 		'default' => [
 			'bureaucrat' => [ 'accountcreator', 'sysop', 'interface-admin', 'bureaucrat', 'bot', 'confirmed', 'reviewer', 'editor', 'flow-bot' , 'ipblock-exempt', 'translation-admin'],
-			'sysop' => [ 'rollbacker', 'editor', 'reviewer', 'confirmed', 'autoreview', 'patroller', 'rollbacker', 'transwiki' ],
+			'sysop' => [ 'rollbacker', 'editor', 'reviewer', 'confirmed', 'autoreview', 'patroller', 'transwiki', 'ipblock-exempt', 'autopatrolled' ],
 		],
 		'+idwiki' => [
 			'bureaucrat' => [ 'sysop' ],
